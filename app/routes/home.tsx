26a73cb2b53e59183ff29087e7cbce04a0e77463
Chetanya Hans <chetanyahans@gmail.com>
2026-04-19 19:14:06 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import type { Route } from "./+types/home";
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
 import Navbar from "../components/Navbar";
 import { resumes } from "../../Constants/index";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
 
 
 export function meta({ }: Route.MetaArgs) {
@@ -12,6 +15,19 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+
+
+   const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!auth.isAuthenticated) {
+            navigate('/auth?next=/');
+        }
+    }, [auth.isAuthenticated]);
+
+
   return <main className="bg-[url('../assets/images/bg-main.svg')] bg-cover">
     <Navbar />
 
